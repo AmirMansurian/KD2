043d4a54a53e9ecaba09c139341f07c01c4a7952
amir <42087527+AmirMansurian@users.noreply.github.com>
2022-08-16 01:37:44 +0430

    Update train_kd.py

--- train_kd.py
+++ train_kd.py
@@ -121,7 +121,7 @@ class Trainer(object):
                 image, target = image.cuda(), target.cuda()
             self.scheduler(optimizer, i, epoch, self.best_pred)
             optimizer.zero_grad()
-            output, loss_distill = self.d_net(image)
+            output, dist_loss, loss_distill = self.d_net(image)
             #print(loss_kd/ batch_size)
 
             loss_seg = self.criterion(output, target)
@@ -130,7 +130,7 @@ class Trainer(object):
            # print('################################')
             #loss = loss_seg + loss_distill.sum() / batch_size * 1e-5
             #loss = loss_seg + loss_kd
-            loss = loss_seg + 10 * loss_distill
+            loss = loss_seg + 100 * loss_distill + dist_loss
 
             loss.backward()
             optimizer.step()
